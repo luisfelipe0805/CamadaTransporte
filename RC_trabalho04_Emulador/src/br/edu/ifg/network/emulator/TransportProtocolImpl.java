371803f5841c3e13a0e1d5be84d149d577049241
Luis Felipe <32879529+luisfelipe0805@users.noreply.github.com>
2018-11-04 08:41:13 -0200

    Update TransportProtocolImpl.java

--- RC_trabalho04_Emulador/src/br/edu/ifg/network/emulator/TransportProtocolImpl.java
+++ RC_trabalho04_Emulador/src/br/edu/ifg/network/emulator/TransportProtocolImpl.java
@@ -9,31 +9,34 @@ public class TransportProtocolImpl extends TransportProtocol {
 
 	private int A=0;
 	private int B=0;
-	private int tamanhoPacket=20;	
+	private int TamanhoPacket=20;	
+	private Packet pacote_A;
+	private Packet pacote_B;
 	
 	
 	@Override
 	public void A_output(Message message) {
-		Packet pacote = new Packet();
+		
 		//tranferir string para um pacote
-		pacote=PassarStringParaPacket(message);
+		pacote_A=PassarStringParaPacket(message);
 		
 		//Montagem do checksum
-		pacote.setChecksum(CalcularCheackSum (pacote));
-		
-		
+		pacote_A.setChecksum(CalcularCheackSum(pacote_A));
+				
 		//Uso do seq
 		//uso da variavel privada A como parametro pra montar o valor do seq;
-		CalcularValorSeqsum();
-			
-		CalcularValoracknum();
-		pacote.setSeqnum(A);
+		CalcularValorSeqnum();
+		pacote_A.setSeqnum(A);
+		
+		//enviando pacote		
+		System.out.println("--A:  Enviando pacote com seq "+pacote_A.getSeqnum() +" ");
+		networkEmulator.tolayer3(0, pacote_A);
+		
+		//StartTimer
+		//A_timerinterrupt();
 		
 		
-		System.out.println("Enviando pacote com seq "+pacote.getSeqnum() +" ");
 		
-		
-		System.out.println("start a new timer!");
 		
 	}
 
@@ -43,27 +46,81 @@ public class TransportProtocolImpl extends TransportProtocol {
 		
 	}
 
+	/**
+	 * Funcao responsavel pela recepsao do pacote vinda do lado B,
+	 * Onde sera validado se o pacote foi recebido corretamente 
+	 */
 	@Override
 	public void A_input(Packet packet) {
-		// TODO Auto-generated method stub
+		if(packet.getPayload()!=null) {
+			System.out.println("--A:  Confirmacao do pacote recebida com sucesso");
+			if(packet.getAcknum()==A+TamanhoPacket) {
+				System.out.println("--A:  Acknum Correto!!!");
+				//networkEmulator.stoptimer(0);
+				
+				//Atualizacoa do pacote 
+				A=packet.getAcknum();
+				System.out.println("\n\n");
+			}else {
+				System.out.println("--A:  Acknum Incorreto!!!");
+				System.out.println("\n\n");
+				System.out.println("--A:  Iniciar Reenvio do pacote");
+				networkEmulator.tolayer3(0, pacote_A);
+				//networkEmulator.stoptimer(0);
+			}
+		}else {
+			System.out.println("--A:  Este pacote nao confirmacao de recebimento");
+		}
 		
 	}
 
 	@Override
 	public void A_timerinterrupt() {
-		// TODO Auto-generated method stub
-		
+//		starttimer();
+		System.out.println("--A:  start a new timer!");
+		networkEmulator.starttimer(0, 1000000000);
 	}
-
+	
+	/**
+	 * Funcao para inicializar o sistema do lado A
+	 */
 	@Override
 	public void A_init() {
 		// TODO Auto-generated method stub
-		
+		System.out.println("--A:  Iniciando sistema lado A \n");
+		pacote_A = new Packet();
 	}
 
+	
+	/**
+	 * Funcao responsavel pela recepsao do pacote vinda do lado A,
+	 * Onde sera validado se seria o pacote correto a ser recebido
+	 */
 	@Override
 	public void B_input(Packet packet) {
-		// TODO Auto-generated method stub
+		//valida o seq do pacote recebido
+		if(packet.getSeqnum()==B) {
+			System.out.println("--B:  Pacote seq: "+packet.getSeqnum()+" recebido com sucesso");
+			
+			//com o seq valido a funcao ira validar a sua string
+			if(ValidaCheackSum(packet)) {
+				System.out.println("--B:  Pacote validado com sucesso");
+				
+				//com o pacote valido ira calcular o acknum
+				pacote_B.setAcknum(CalcularValoracknum());
+			}else {
+				System.out.println("--B:  Pacote corrompido");
+				pacote_B.setAcknum(B);
+			}
+			
+		}else {
+			System.err.println("--B:  Pacote com seq incorreto, "+packet.getSeqnum());
+			pacote_B.setAcknum(B);
+		}
+				
+		//envio da confirmacoa do pacote recebido
+		System.out.println("--B:  Envio da confirmacao do recebimento");
+		networkEmulator.tolayer3(1, pacote_B);		
 		
 	}
 
@@ -73,14 +130,16 @@ public class TransportProtocolImpl extends TransportProtocol {
 		
 	}
 
+	/**
+	 * Funcao para inicializar o sistema do lado B
+	 */
 	@Override
 	public void B_init() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("--B:  Iniciando sistema lado B \n");
+		pacote_B = new Packet();
 	}
 	
-	
-	
+		
 	/**
 	 * Funcao para passar a informacao do atributo Data uma Message para o atributo Payload de um Packet
 	 * @param Um Objeto do tipo Message 
@@ -93,7 +152,6 @@ public class TransportProtocolImpl extends TransportProtocol {
 		}		
 		return pacote;
 	}
-	
 	
 	/**
 	 * Funcao que calcula o somatorio de todo vetor de char
@@ -108,6 +166,16 @@ public class TransportProtocolImpl extends TransportProtocol {
 			int numero;
 			numero=pacote.getPayload(ipayload);
 			result=result + numero;
+			
+			
+			//anotaçao pra converter em binario
+		////conversao de em binaria de cada char
+		//int valorBinario = pacote.getPayload(ipayload);
+		//String binaria = Integer.toBinaryString(valorBinario);
+		////System.out.println(binaria);
+		//
+		//int i = Integer.parseInt(binaria);
+		//System.out.println(i);
 		}	
 		return result;
 	}
@@ -116,7 +184,7 @@ public class TransportProtocolImpl extends TransportProtocol {
 	 * Calcular valor do atributo seqSum de um pacote
 	 * @return valor do tipo integer 
 	 */
-	private int CalcularValorSeqsum() {
+	private int CalcularValorSeqnum() {
 		int result=A;
 		return result;
 	}
@@ -126,23 +194,30 @@ public class TransportProtocolImpl extends TransportProtocol {
 	 * @return valor do tipo integer 
 	 */
 	private int CalcularValoracknum() {
-		B=B+tamanhoPacket;
+		B=B+TamanhoPacket;
 		int result=B;
-		A=B;
 		return result;
 	}
+	
+	/**
+	 * Valida o valor do CheackSum com o valor da String Payload
+	 * para isso ele utiliza fucao CalcularCheackSum 
+	 * @param packet to tipo pocte 
+	 * @return true para o pacote com sua string valida false para o pacote com String nao valida
+	 */
+	private boolean ValidaCheackSum(Packet packet) {
+		//Validando o payload do pacote 
+			if(CalcularCheackSum(packet)==packet.getChecksum()) {
+				return true;
+			}else {
+				return false;
+			}
+		
+	}
+	
+	
 }
 
 
 
 
-
-
-
-////conversao de em binaria de cada char
-//int valorBinario = pacote.getPayload(ipayload);
-//String binaria = Integer.toBinaryString(valorBinario);
-////System.out.println(binaria);
-//
-//int i = Integer.parseInt(binaria);
-//System.out.println(i);
